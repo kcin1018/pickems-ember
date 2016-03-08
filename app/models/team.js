51e291bf04182c974a9239ee901cb388b75ef4f2
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-03-07 22:29:45 -0500

    Updating registration to not user validations

--- app/models/team.js
+++ app/models/team.js
@@ -1,21 +1,8 @@
 import DS from 'ember-data';
-import { validator, buildValidations } from 'ember-cp-validations';
 
 const { attr, belongsTo } = DS;
 
-const Validations = buildValidations({
-  first_name: {
-    debounce: 500,
-    validators: [
-      validator('presence', true),
-      validator('length', {
-        max: 25
-      })
-    ]
-  }
-});
-
-export default DS.Model.extend(Validations, {
+export default DS.Model.extend({
   name: attr('string'),
   paid: attr('boolean'),
   slug: attr('string'),
