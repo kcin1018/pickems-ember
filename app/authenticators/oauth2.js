e2fec633c585b0d94bee0760242e7f6cd74dbf22
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-03-09 09:10:09 -0500

    Fixing some errors

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import OAuth2PasswordGrantAuthenticator from 'ember-simple-auth/authenticators/oauth2-password-grant';
 import Config from 'pickems/config/environment';
 
@@ -9,9 +10,6 @@ export default OAuth2PasswordGrantAuthenticator.extend({
 
   restore(data) {
     return new RSVP.Promise((resolve, reject) => {
-      let now = (new Date()).getTime();
-      let refreshAccessTokens = this.get('refreshAccessTokens');
-
       // check for valid access token
       if (isEmpty(data.data.access_token)) {
         reject();
