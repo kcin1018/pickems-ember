5df9fafa284d7e25ebfea7fe26ae902471040f62
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-04-08 09:53:58 -0400

    Added Authentication methods

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -11,6 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    header('Access-Control-Allow-Origin: http://localhost:4200');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, PATCH, DELETE');
+
+    $api->group(['namespace' => 'App\Http\Controllers\Api'], function ($api) {
+        // Auth
+        $api->post('auth/login', 'Auth\AuthController@postLogin');
+        $api->post('auth/token-refresh', 'Auth\AuthController@refreshToken');
+        $api->post('users', 'Auth\UsersController@store');
+
+        // Protected methods (require auth)
+        $api->group(['middleware' => 'api.auth'], function ($api) {
+
+        });
+
+        // Public methods
+
+    });
 });
+
+Route::any('{catchall}', function () {
+    return view('index');
+})->where('catchall', '(.*)');
